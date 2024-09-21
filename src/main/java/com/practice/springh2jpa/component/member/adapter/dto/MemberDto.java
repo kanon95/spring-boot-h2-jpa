39e300037451d3a1ca334663a98618886cf6eff9
kanon95 <kanon95@gmail.com>
2024-09-21 20:32:51 +0900

    - Swagger UI 파라미터 설정 #1

--- src/main/java/com/practice/springh2jpa/component/member/adapter/dto/MemberDto.java
+++ src/main/java/com/practice/springh2jpa/component/member/adapter/dto/MemberDto.java
@@ -1,6 +1,7 @@
 package com.practice.springh2jpa.component.member.adapter.dto;
 
 import com.practice.springh2jpa.component.member.domain.entity.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.io.Serializable;
 import java.time.LocalDate;
 
@@ -12,24 +13,31 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
+@Schema(name = "MemberDto", description = "사용자 정보 Parameter 객체 - 사용자 Parameter를 기술합니다.")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class MemberDto implements Serializable {
+  @Schema (description = "사용자 회원번호. 등록 시, 부여됨. 외부에서 설정하지 않음.")
   private Long memberKey;
 
+  @Schema (description = "사용자 회원ID", example = "test1")
   @NotBlank
   private String memberId;
 
+  @Schema (description = "사용자 이름", example = "테스터1")
   @NotBlank
   @Size(min=1, max=50)
   private String name;
 
+  @Schema (description = "사용자 email", example = "test1@site.com")
   @NotBlank
   private String email;
 
+  @Schema (description = "사용자 나이", example = "31")
   private int age;
 
+  @Schema (description = "생년월일", example = "2024-09-11")
   private LocalDate birthday;
 
   public void setValue(Long memberKey, String memberId, String name, String email, int age, LocalDate birthday) {
@@ -42,14 +50,13 @@ public class MemberDto implements Serializable {
   }
 
   public static MemberDto from(Member member) {
-    MemberDto memberDto = new MemberDto( member.getMemberKey(),
+   return new MemberDto( member.getMemberKey(),
         member.getMemberId(),
         member.getName(),
         member.getEmail(),
         member.getAge(),
         member.getBirthday()
     );
-    return memberDto;
   }
 
   public Member toDomain() {
--- src/main/java/com/practice/springh2jpa/component/member/controller/MemberController.java
+++ src/main/java/com/practice/springh2jpa/component/member/controller/MemberController.java
@@ -3,9 +3,17 @@ package com.practice.springh2jpa.component.member.controller;
 import com.practice.springh2jpa.component.member.adapter.dto.MemberDto;
 import com.practice.springh2jpa.component.member.domain.entity.Member;
 import com.practice.springh2jpa.component.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,16 +27,68 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/member")
+@Tag(name = "사용자", description = "사용자 CRUD 서비스 제공 <br/> API 설명")
 public class MemberController {
+
   private final MemberService memberService;
 
+  @Operation(
+      summary = "사용자 조회",
+      description = "사용자ID(memberId)로 사용자를 조회 합니다.",
+      responses = {
+          @ApiResponse( responseCode = "200", description = "사용자 조회 성공"),
+          @ApiResponse( responseCode = "404", description = "사용자 조회 실패")
+      }
+  )
+  @Parameters ({
+      @Parameter(name = "dummy1", description = "더미 파라미터1 - queryString 예시1", example = "dummyValue1"),
+      @Parameter(name = "dummy2", description = "더미 파라미터2 - queryString 예시2", example = "dummyValue2")
+  })
   @GetMapping("{memberId}")
-  public ResponseEntity<MemberDto> find(@PathVariable String memberId ) {
+  public ResponseEntity<MemberDto> find(
+      @Parameter (description = "사용자ID(memberId)" , example = "test1")
+      @PathVariable String memberId ) {
     Member member = memberService.findByMemberId(memberId);
     MemberDto dto = MemberDto.from(member);
     return ResponseEntity.ok(dto);
   }
 
+  @Operation(
+      summary = "사용자 등록",
+      description = "사용자를 등록합니다.",
+      requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+          content = @Content(
+              mediaType = MediaType.APPLICATION_JSON_VALUE,
+              schema = @Schema( implementation = MemberDto.class ),
+              examples = {
+                  @ExampleObject(name = "memberDto 회원등록 예제1", value = """
+                      {
+                        "memberId" : "test1",
+                        "name" : "테스터1",
+                        "email" : "test1@site.com",
+                        "age" : "31",
+                        "birthday" : "2024-09-21"
+                      }
+                      """,
+                  description = "신규로 회원을 등록합니다.#1"),
+                  @ExampleObject(name = "memberDto 회원등록 예제2", value = """
+                      {
+                        "memberId" : "test2",
+                        "name" : "테스터2",
+                        "email" : "test2@site.com",
+                        "age" : "32",
+                        "birthday" : "2024-09-22"
+                      }
+                      """,
+                      description = "신규로 회원을 등록합니다.#2"),
+
+              }
+          )
+      ),
+      responses = {
+          @ApiResponse( responseCode = "200", description = "사용자 등록 성공")
+      }
+  )
   @PostMapping
   public ResponseEntity<MemberDto> register(@Valid @RequestBody MemberDto memberDto) {
     Member member = memberService.register(memberDto);
